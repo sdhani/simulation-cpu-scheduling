3f11ba0fea6ef8a2f04ee27956705f2d2082ee9b
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-03 18:02:28 -0400

    cleaned up process header

--- Process.h
+++ Process.h
@@ -6,21 +6,21 @@
 class Process
 {
 private: 
-    std::string process_type_; /* Common Process or RT-process */
-    int memory_size_ = 0;
-    int pid_ = 0; /* Don't reassign PID's */
+  std::string process_type_; /* Common Process or RT-process */
+  int memory_size_ = 0;
+  int pid_ = 0; /* Don't reassign PID's */
 
 public: 
-    Process(); // default constructor
-    Process(const std::string& process_type_, const std::string& memory_size_, const int& pid_);
+  Process(); // default constructor
+  Process(const std::string& process_type, const int& memory_size, const int& pid);
    
     
-    void setProcessType(std::string process_type); // sets process_type
-    void setMemorySize(int memory_size); // sets memory_size
-    void setPID(int pid); // sets pid EXPLORE AUTO-INCREMENT/COUNT OF PROCESSES "BAG"
-    std::string getProcessType() const; // @return process type
-    std::string getMemorySize() const; // @return memory size
-    std::string getPID() const; // @return pid
+  void setProcessType(std::string process_type); // sets process_type
+  void setMemorySize(int memory_size); // sets memory_size
+  void setPID(int pid); // sets pid EXPLORE AUTO-INCREMENT/COUNT OF PROCESSES "BAG"
+  std::string getProcessType() const; // @return process type
+  int getMemorySize() const; // @return memory size
+  int getPID() const; // @return pid
 
 }; //end Process
 
