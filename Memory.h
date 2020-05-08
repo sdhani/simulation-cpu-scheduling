0812e7bbe2228e2ab8877eee1e9e0f0ceee6321f
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-08 09:17:10 -0400

    updated memory to include mem range class for processes

---
+++ Memory.h
@@ -0,0 +1,25 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <iostream>
+#include <list>
+#include "MemRange.h"
+
+class Memory
+{
+private:
+  std::list<MemRange> continguous_memory_; /* Some data type that shows which processes using how much memory */
+  long long int memory_size_ = 0;
+
+public:
+  Memory(); // default constructor
+  Memory(const long long int &memory_size);
+
+  void setMemorySize(int memory_size);           // sets size of RAM memory
+  long long int getMemorySize() const;           // @return size of RAM memory
+  std::list<MemRange> getMemorySnapshot() const; // @return memory list
+  void printMemory() const;                      // print out memory snapshot
+
+}; //end Memory
+
+#endif // MEMORY_H
