88b50ed0f4b2608c120a8de2a54529de54150801
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-08 11:10:45 -0400

    added function prototypes to simulation

--- Simulation.cpp
+++ Simulation.cpp
@@ -2,11 +2,11 @@
 
 Simulation::Simulation() {} // default constructor
 
-Simulation::Simulation(const Memory &ram_memory, /* const CPU &cpu, */ const ReadyQueue &ready_queue /*, const IOQueue &io_queue, const HardDisks &hard_disks */)
+Simulation::Simulation(const Memory &ram_memory /*, const CPU &cpu, const ReadyQueue &ready_queue, const IOQueue &io_queue, const HardDisks &hard_disks */)
 {
   RAM_MEMORY_ = ram_memory;
   // CPU_ = cpu;
-  READY_QUEUE_ = ready_queue;
+  // READY_QUEUE_ = ready_queue;
   // IO_QUEUE_ = io_queue;
   // HARD_DISKS_ = hard_disks;
 }
@@ -18,7 +18,7 @@ void Simulation::setRAM(const Memory &ram_memory)
 }
 
 // set CPU
-// void Simulation::setCPU(const CPU &cpu){}
+// void Simulation::setCPU(const CPU &cpu){ CPU_ = cpu; }
 
 // sets ready_queue
 void Simulation::setReadyQueue(const ReadyQueue &ready_queue)
@@ -27,10 +27,10 @@ void Simulation::setReadyQueue(const ReadyQueue &ready_queue)
 }
 
 // sets ready_queue
-// void Simulation::setIOQueue(const IOQueue &io_queue){}
+// void Simulation::setIOQueue(const IOQueue &io_queue){ READY_QUEUE_ = ready_queue; }
 
 // sets ready_queue
-// void Simulation::setHardDisks(const HardDisks &hard_disks){}
+// void Simulation::setHardDisks(const HardDisks &hard_disks){ HARD_DISKS_ = hard_disks; }
 
 // @return Memory
 Memory Simulation::getMemory() const
@@ -39,33 +39,35 @@ Memory Simulation::getMemory() const
 }
 
 // @return CPU
-// CPU Simulation::getCPU() const {}
+// CPU Simulation::getCPU() const { return CPU_; }
 
 // @return Ready Queue
 ReadyQueue Simulation::getReadyQueue() const
 {
+  return READY_QUEUE_;
 }
 
 // @return I/O Queue
-// IOQueue Simulation::getIOQueue() const {}
+// IOQueue Simulation::getIOQueue() const { return IO_QUEUE_; }
 
 // @return Hard Disks
-// HardDisks Simulation::getHardDisks() const {}
+// HardDisks Simulation::getHardDisks() const { return HARD_DISKS_; }
 
 // get memory snapshot
 void Simulation::printSM()
 {
-  // (Shows the state of memory. Show the range of memory addresses used by each process in the system.)
+  std::cout << "Shows the state of memory. Show the range of memory addresses used by each process in the system." << std::endl;
+  RAM_MEMORY_.printMemory();
 }
 
 // get hard-disk and i/o snapshot
 void Simulation::printSI()
 {
-  // (Shows what processes are currently using the hard disks and what processes are waiting to use them. For each busy hard disk show the process that uses it and show its I/O-queue. The enumeration of hard disks starts from 0.)
+  std::cout << "Shows what processes are currently using the hard disks and what processes are waiting to use them. For each busy hard disk show the process that uses it and show its I/O-queue. The enumeration of hard disks starts from 0." << std::endl;
 }
 
 // get ready-queue and cpu snapshot
 void Simulation::printSR()
 {
-  // (Shows what process is currently using the CPU and what processes are waiting on both levels of the ready-queue.)
+  std::cout << "Shows what process is currently using the CPU and what processes are waiting on both levels of the ready-queue." << std::endl;
 }
--- Simulation.h
+++ Simulation.h
@@ -18,8 +18,8 @@ private:
   // HardDisks HARD_DISKS_;
 
 public:
-  Simulation();                                                                            // default constructor
-  Simulation(const Memory &ram_memory, /* const CPU &cpu, */ const ReadyQueue &ready_queue /*, 
+  Simulation();                       // default constructor
+  Simulation(const Memory &ram_memory /*, const CPU &cpu,  const ReadyQueue &ready_queue , 
   const IOQueue &io_queue, const HardDisks &hard_disks */
   );
 
@@ -33,6 +33,14 @@ public:
   ReadyQueue getReadyQueue() const; // @return Ready Queue
   // IOQueue getIOQueue() const; // @return I/O Queue
   // HardDisks getHardDisks() const; // @return Hard Disks
+
+  void interpretCommand(std::string line);               // parses command line instructions
+  void executeACommand(const int &process_memory_size);  // executes A # command, create common process
+  void executeARCommand(const int &process_memory_size); // executes AR # command, create real-time process
+  void executeQCommand();                                // executes Q command; ends Round-Robin Time Slice
+  void executeTCommand();                                // executes t command; terminate currently running process
+  void executedCommand();                                // executes d # command; process that currently uses the CPU requests the hard disk #
+  void executeDCommand();                                // executes D # command; process on hard disk # has finished work; assign next process that wants hard disk # to hard disk
 
   void printSM(); // get memory snapshot (Shows the state of memory. Show the range of memory addresses used by each process in the system.)
   void printSI(); // get hard-disk and i/o snapshot (Shows what processes are currently using the hard disks and what processes are waiting to use them. For each busy hard disk show the process that uses it and show its I/O-queue. The enumeration of hard disks starts from 0.)
