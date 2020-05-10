4c691a9261fdb84eec2852910b91b1f887581b62
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-09 22:15:59 -0400

    simplified add Process logic to memory, cleaned up code

--- Memory.cpp
+++ Memory.cpp
@@ -1,11 +1,18 @@
 #include "Memory.hpp"
-#include <iomanip>
+
+struct MemBlock
+{
+  int start_;
+  int end_;
+  int pid_;
+};
 
 Memory::Memory() {}
 
 Memory::Memory(const long long int &memory_size)
 {
   memory_size_ = memory_size;
+  free_memory_ = memory_size;
 }
 
 // sets size of RAM memory
@@ -21,27 +28,148 @@ long long int Memory::getMemorySize() const
 }
 
 // @return memory list
-std::list<MemRange> Memory::getContiguousMemory() const
+// std::vector<MemBlock> Memory::getContiguousMemory() const
+// {
+//   return continguous_memory_;
+// }
+
+// @return true if add process to memory is successful
+bool Memory::addProcessToMemory(const Process &p)
 {
-  return continguous_memory_;
+  int p_size = p.getMemorySize();
+  if (free_memory_ >= p_size)
+  {
+    for (unsigned i = 0; i < continguous_memory_.size(); i++)
+    {
+      MemBlock m = continguous_memory_[i];
+      if (!m.active_ && m.memory_chunk_ >= p_size)
+      {
+        int new_end = (m.start_ + p_size);
+        MemBlock new_m = {m.start_, new_end, p.getPID(), p_size, true};
+
+        continguous_memory_[i].start_ = new_end; // Shift start of memory hole to end of new process
+        continguous_memory_[i].memory_chunk_ = continguous_memory_[i].end_ - new_end;
+        continguous_memory_.insert(continguous_memory_.begin() + i, new_m);
+
+        free_memory_ -= p_size; // allocate memory
+        return true;
+      }
+    }
+
+    /* Append to front of memory */
+    if (continguous_memory_.empty() && free_memory_ >= p_size)
+    {
+      MemBlock new_m = {0, (p_size + -1), p.getPID(), p_size, true};
+      continguous_memory_.push_back(new_m);
+      free_memory_ -= p_size; // allocate memory
+      return true;
+    }
+    /* Append to the end of memory */
+    else if ((memory_size_ - continguous_memory_.back().end_) >= p_size)
+    {
+      int end = continguous_memory_.back().end_; // end of last process in memory
+      MemBlock new_m = {end, (end + p_size), p.getPID(), p_size, true};
+      continguous_memory_.push_back(new_m);
+      free_memory_ -= p_size; // allocate memory
+      return true;
+    }
+  }
+
+  std::cout << "Not enough memory to add new process." << std::endl;
+  return false;
+}
+
+// @return true if remove process from memory is successful
+bool Memory::removeProcessFromMemory(const Process &p)
+{
+  for (unsigned i = 0; i < continguous_memory_.size(); i++)
+  {
+    if (continguous_memory_[i].pid_ == p.getPID())
+    {
+      continguous_memory_[i].active_ = false; /* Process Terminated */
+      free_memory_ += continguous_memory_[i].memory_chunk_;
+      return true;
+    }
+  }
+  return false; /* process does not exist */
 }
 
 // print out memory snapshot
 void Memory::printMemory() const
 {
-  const char separator = ' ';
-  const int pidWidth = 6;
-  const int rangeWidth = 16;
-
-  std::cout << std::left << std::setw(pidWidth) << std::setfill(separator) << "PID" << std::endl;
-  std::cout << std::left << std::setw(rangeWidth) << std::setfill(separator) << "MemRange" << std::endl;
-
-  for (const auto &mem_range : continguous_memory_)
+  if (continguous_memory_.empty())
   {
-    std::cout << std::left << std::setw(pidWidth) << std::setfill(separator)
-              << mem_range.getProcess().getPID() << std::endl;
+    std::cout << "memory empty" << std::endl;
+  }
 
-    std::cout << std::left << std::setw(rangeWidth) << std::setfill(separator)
-              << mem_range.getStart() << "-" << mem_range.getEnd() << std::endl;
+  for (auto &m : continguous_memory_)
+  {
+    std::cout << m.pid_ << "|  "
+              << m.start_ << "-" << m.end_ << "| " << (m.active_ ? "A" : "EMPTY") << std::endl;
   }
 }
+
+// // @return true if add process to memory is successful
+// bool Memory::addProcessToMemory(const Process &p)
+// {
+//   for (unsigned int i = 0; i < continguous_memory_.size(); i++)
+//   {
+//     MemRange &m = continguous_memory_[i];
+//     if (!m.isActive() && (m.getProcess().getMemorySize() >= p.getMemorySize()))
+//     {
+//       MemRange new_process;
+//       new_process.setStart(m.getStart());                   /* set new process start to start of memory hole */
+//       new_process.setEnd(m.getStart() + p.getMemorySize()); /* set new process end to start + new process's size */
+//       new_process.setActive(true);
+
+//       /* insert new process into vector of continguous memory */
+//       continguous_memory_.insert(continguous_memory_.begin() + i, new_process);
+//       memory_size_ -= p.getMemorySize();
+
+//       if (m.getProcess().getMemorySize() != p.getMemorySize()) /* if memory hole remains */
+//       {
+//         m.setStart(new_process.getEnd()); /* its end range stays the same */
+//       }
+//       return true;
+//     }
+//     else if ((i == continguous_memory_.size() - 1) || continguous_memory_.size()) /* if no memory holes big enough for new process, try appending it to end */
+//     {
+//       if ((memory_size_ - m.getEnd()) >= p.getMemorySize())
+//       {
+//         MemRange new_process;
+//         new_process.setStart(m.getEnd());                   /* set new process start to start of memory hole */
+//         new_process.setEnd(m.getEnd() + p.getMemorySize()); /* set new process end to start + new process's size */
+//         new_process.setActive(true);
+
+//         /* insert new process into vector of continguous memory */
+//         continguous_memory_.insert(continguous_memory_.begin() + i, new_process);
+//         memory_size_ -= p.getMemorySize();
+//         return true;
+//       }
+//       else
+//       {
+//         std::cout << "add fail, not enough memory" << std::endl;
+//         return false;
+//       }
+//     }
+//   }
+
+//   if (continguous_memory_.empty() && (memory_size_ >= p.getMemorySize())) /* if no fit between processes in memory, try appending it to end */
+//   {
+//     MemRange new_process;
+//     new_process.setStart(0);                    /* set new process start to start of memory hole */
+//     new_process.setEnd(-1 + p.getMemorySize()); /* set new process end to start + new process's size */
+//     new_process.setActive(true);
+
+//     /* insert new process into vector of continguous memory */
+//     continguous_memory_.push_back(new_process);
+//     memory_size_ -= p.getMemorySize();
+//     return true;
+//   }
+//   else
+//   {
+//     std::cout << "add fail 1" << std::endl;
+//     return false;
+//   }
+//   return true; /* Added in the loop, check memory before add */
+// }
--- Memory.hpp
+++ Memory.hpp
@@ -2,24 +2,34 @@
 #define MEMORY_H
 
 #include <iostream>
-#include <list>
 #include <vector>
 #include "MemRange.hpp"
 
 class Memory
 {
 private:
-  std::list<MemRange> continguous_memory_; /* Some data type that shows which processes using how much memory */
-  long long int memory_size_ = 0;
+  struct MemBlock
+  {
+    int start_;
+    int end_;
+    int pid_;
+    int memory_chunk_;
+    bool active_;
+  };
+  std::vector<MemBlock> continguous_memory_; /* Some data type that shows which processes using how much memory */
+  long long int memory_size_;
+  long long int free_memory_;
 
 public:
   Memory(); // default constructor
   Memory(const long long int &memory_size);
 
-  void setMemorySize(const int &memory_size);      // sets size of RAM memory
-  long long int getMemorySize() const;             // @return size of RAM memory
-  std::list<MemRange> getContiguousMemory() const; // @return memory list
-  void printMemory() const;                        // print out memory snapshot
+  void setMemorySize(const int &memory_size);        // sets size of RAM memory
+  long long int getMemorySize() const;               // @return size of RAM memory
+  bool addProcessToMemory(const Process &p);         // @return true if add process to memory is successful
+  bool removeProcessFromMemory(const Process &p);    // @return true if remove process from memory is successful
+  std::vector<MemBlock> getContiguousMemory() const; // @return memory list
+  void printMemory() const;                          // print out memory snapshot
 
 }; //end Memory
 
